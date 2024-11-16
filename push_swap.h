1a9f7bc9a269f07286674ad893615c43e3ebc08c
pabloojdr <pabloojdr@gmail.com>
2024-11-16 20:13:04 +0100

    feat: :sparkles: add definitions of check functions

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pcampoy- <pcampoy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 16:57:38 by pcampoy-          #+#    #+#             */
-/*   Updated: 2024/11/16 17:47:43 by pcampoy-         ###   ########.fr       */
+/*   Updated: 2024/11/16 20:11:39 by pcampoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 # define PUSH_SWAP_H
 # include "ft_printf/ft_printf.h"
 # include "libft/libft.h"
+
+typedef struct s_stack
+{
+	int				value;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_stack	*next;
+}	t_stack;
+
+/// @brief Checks if the elements of a string are numbers
+/// @param numbers String to check
+void	ft_checknumbers(char *numbers);
+
+/// @brief Checks whether a string only contains no-repeating numbers
+/// @param numbers String to check
+void	ft_checkerrorssimple(char *numbers);
+
+/// @brief Checks whether the received arguments contain repeating numbers 
+/// @param numarg Number of arguments
+/// @param args Arguments to check
+void	ft_checkerrorsmult(int numarg, char *args[]);
 
 #endif
