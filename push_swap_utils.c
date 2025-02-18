0ddbefcb95e137e4da991d9379100ed55e5b174d
pabloojdr <pabloojdr@gmail.com>
2025-02-18 17:44:31 +0100

    feat: :sparkles: change checkers to push_swap_utils.c

--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,7 +6,7 @@
 /*   By: pcampoy- <pcampoy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 15:36:47 by pcampoy-          #+#    #+#             */
-/*   Updated: 2025/02/17 15:54:46 by pcampoy-         ###   ########.fr       */
+/*   Updated: 2025/02/18 17:21:27 by pcampoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,31 @@ int	checkdup(char **arg)
 		}
 		i++;
 	}
-	ft_printf("Pasa de aquí");
 	return (0);
+}
+
+char	**check(int argc, char *argv[])
+{
+	int		allocated;
+	char	**arguments;
+
+	allocated = 0;
+	arguments = NULL;
+	if (argc <= 1 || (argc == 2 && checknargs(argv[1]) <= 1) 	// Comprobamos los casos en los que no se pasan argumentos, se pasan argumentos y ninguno es un número, y
+		|| checkalchar(&argv[1]))
+		exit(1);												// Sale del programa y devuelve el control sin hacer nada
+	if (argc == 2 && checknargs(argv[1]) > 1)
+	{
+		arguments = ft_split(argv[1], ' ');						// Devuelve un array de arrays que contiene a los números por separado como chars
+		allocated = 1;
+	}															// Comprobamos que lo pasan como cadena y que la cadena contiene más de un numero
+	else if (argc > 2)											// Si el número de argumentos pasados es más de dos, los numeros no vienen como cadena y se asignan directamente
+		arguments = &argv[1];
+	if (checkerr(arguments) == -1)
+	{
+		if (allocated == 1)
+			ft_free(arguments);
+		exit(1);
+	}
+	return (arguments);
 }
