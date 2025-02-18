7a978770996637bf384b40e59b0f812ba3c748d7
pabloojdr <pabloojdr@gmail.com>
2025-02-18 23:05:18 +0100

    feat: :sparkles: finish asigindex function

--- stack_more_utils.c
+++ stack_more_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stack_more_utils.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcampoy- <pcampoy-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pabloojdr <pabloojdr@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:59:23 by pcampoy-          #+#    #+#             */
-/*   Updated: 2025/02/18 18:07:29 by pcampoy-         ###   ########.fr       */
+/*   Updated: 2025/02/18 22:51:09 by pabloojdr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,21 @@
 void	asignindex(t_stack *s)
 {
 	t_stack	*aux;
+	t_stack	*aux2;
 	int		index;
 
 	aux = s;
-	index = 1;
-	while (s != NULL)
+	while (aux != NULL)
 	{
-		s->index = index;
-		index++;
-		s = s->next;
+		index = 1;
+		aux2 = s;
+		while (aux2 != NULL)
+		{
+			if (aux2->value < aux->value)
+				index++;
+			aux2 = aux2->next;
+		}
+		aux->index = index;
+		aux = aux->next;
 	}
-	s = aux;
 }
