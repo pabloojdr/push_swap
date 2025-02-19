2d8f77c7a793f2e1062acd2ca0fdbbf8209bfdde
pabloojdr <pabloojdr@gmail.com>
2025-02-19 18:12:06 +0100

    feat: :sparkles: add new functions to calculate indexes, positions and targets

--- stack_more_utils.c
+++ stack_more_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stack_more_utils.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pabloojdr <pabloojdr@student.42.fr>        +#+  +:+       +#+        */
+/*   By: pcampoy- <pcampoy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:59:23 by pcampoy-          #+#    #+#             */
-/*   Updated: 2025/02/18 22:51:09 by pabloojdr        ###   ########.fr       */
+/*   Updated: 2025/02/19 17:58:17 by pcampoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,70 @@ void	asignindex(t_stack *s)
 		aux->index = index;
 		aux = aux->next;
 	}
+}
+
+void	asignposition(t_stack *s)
+{
+	t_stack	*aux;
+	int		position;
+
+	position = 0;
+	aux = s;
+	while (aux != NULL)
+	{
+		aux->pos = position;
+		position++;
+		aux = aux->next;
+	}
+}
+
+void	asigntargetposition(t_stack **a, t_stack **b)
+{
+	t_stack	*auxa;
+	t_stack	*auxb;
+
+	auxb = *b;
+	while (auxb != NULL)
+	{
+		auxa = *a;
+		while (auxa != NULL)
+		{
+			if (auxb->index + 1 == auxa->index)
+				auxb->target_pos = auxa->pos;
+			else if (auxb->index == stacksize(*a) + stacksize(*b))
+				auxb->target_pos = getpositionbyindex(getlowestindex(a), a);
+			auxa = auxa->next;
+		}
+		auxb = auxb->next;
+	}
+}
+
+int	getlowestindex(t_stack **a)
+{
+	t_stack	*aux;
+	int		index;
+
+	aux = *a;
+	index = stacksize(*a);
+	while (aux != NULL)
+	{
+		if (aux->index < index)
+			index = aux->index;
+		aux = aux->next;
+	}
+	return (index);
+}
+
+int	getpositionbyindex(int index, t_stack **a)
+{
+	t_stack	*aux;
+
+	aux = *a;
+	while (aux != NULL)
+	{
+		if (aux->index == index)
+			return (aux->pos);
+		aux = aux->next;
+	}
+	return (-1);
 }
